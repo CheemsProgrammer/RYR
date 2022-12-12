18af442391553abf1b6eb0a8501d0f9bc45f5d36
CheemsProgrammer <anthonncrack@gmail.com>
2022-12-12 17:10:27 -0400

    se añadió un cambio de estilos

--- views/js/inicio.js
+++ views/js/inicio.js
@@ -1,7 +1,7 @@
 //esto abre la barra de iconos
 
-const buttonbar = document.getElementById('buttonbar')
-const bar       = document.getElementById('barItem')
+const buttonbar                 = document.getElementById('buttonbar')
+const bar                       = document.getElementById('barItem')
 
 function actionBar(){
 
@@ -13,15 +13,41 @@ buttonbar.addEventListener('click', actionBar)
 
 //esto cierra la barra
 
-const buttonclose = document.getElementById('buttonclose')
+const buttonclose               = document.getElementById('buttonclose')
 
 function actionBarclose(){
 
   bar.className = 'barclose'
-  listitem.className = 'no'
 
 }
 
 buttonclose.addEventListener('click', actionBarclose)
 
+let encendido                   = document.getElementById('encendido')
+let apagado                     = document.getElementById('apagado')
+let background                  = document.getElementById('dark')
+let singinup                    = document.getElementById('registro-inicio-id')
 
+function darkmode() {
+
+  singinup.className            = 'registro-inicio-dark'
+  bar.style.background          = '#fff'
+  background.style.background   = '#212529'
+  apagado.style.display         = 'block'
+  encendido.style.display       = 'none'
+
+}
+
+encendido.addEventListener('click',darkmode)
+
+function lightmode() {
+
+  singinup.className            = 'registro-inicio'
+  bar.style.background          = '#212529'
+  background.style.background   = '#fff'
+  apagado.style.display         = 'none'
+  encendido.style.display       = 'block'
+
+}
+
+apagado.addEventListener('click',lightmode)
